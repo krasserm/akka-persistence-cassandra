06626c2982c06a1e42444e7adaf5928f265463d1
martinzapletal <zapletal-martin@email.cz>
2015-11-23 19:07:37 -0500

    Events by tag publisher example.

--- src/main/scala/akka/persistence/cassandra/query/journal/CassandraReadJournalProvider.scala
+++ src/main/scala/akka/persistence/cassandra/query/journal/CassandraReadJournalProvider.scala
@@ -9,15 +9,7 @@ import scala.util.control.NonFatal
 class CassandraReadJournalProvider(system: ExtendedActorSystem, config: Config) extends ReadJournalProvider {
 
   override val scaladslReadJournal: scaladsl.CassandraReadJournal =
-    try {
-      new scaladsl.CassandraReadJournal(system, config)
-    } catch {
-        case NonFatal(e) =>
-          println("FAILED FAILED")
-          println(e.getMessage)
-          println(e.getStackTrace)
-          throw e
-  }
+    new scaladsl.CassandraReadJournal(system, config)
 
   override val javadslReadJournal: javadsl.CassandraReadJournal =
     new javadsl.CassandraReadJournal(scaladslReadJournal)
